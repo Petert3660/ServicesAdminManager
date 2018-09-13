c5bd69c6d8d851578d0e59b6534ad8e5075b66b4
Petert3660 <39087631+Petert3660@users.noreply.github.com>
2018-09-13 14:17:24 +0100

    master
    Update of help details file and addition of the HelpDialog to display this.

--- src/main/java/com/ptconsultancy/createdgui/MainDialog.java
+++ src/main/java/com/ptconsultancy/createdgui/MainDialog.java
@@ -394,7 +394,8 @@ public class MainDialog extends JFrame {
         // This is the control for the Help/Help menu item
         menuItem40.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-
+                HelpDialog helpDialog = new HelpDialog(tg);
+                GuiHelper.showFrame(helpDialog);
             }
         });
 
